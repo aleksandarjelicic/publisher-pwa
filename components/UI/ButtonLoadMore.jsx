a3b8335ece4830412636a41c8b761f82761c1b6d
Aleksandar Jelicic <aleksandar.jelicic@sourcefabric.org>
2020-11-10 15:46:46 +0100

    Load more button animation

--- components/UI/ButtonLoadMore.jsx
+++ components/UI/ButtonLoadMore.jsx
@@ -1,10 +1,12 @@
 const ButtonLoadMore = ({ onClick, loading }) => {
   return (
     <button
+      className="btn btn--load"
       style={{ margin: "2em auto", display: "block" }}
       onClick={onClick}
       disabled={loading}
     >
+      <span className="btn__spin"></span>
       load more
     </button>
   );
